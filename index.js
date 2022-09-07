0109982497b28a71cb2bf9166b370f35f8602131
Mainulhasan05 <mdrifatbd5@gmail.com>
2022-09-07 07:18:40 +0600

    msg

--- index.js
+++ index.js
@@ -3,24 +3,36 @@ const app = express()
 const mongoose=require("mongoose")
 const port = process.env.PORT || 3000
 const mongodb="mongodb+srv://user:user@cluster0.ayogb.mongodb.net/messmanagement?retryWrites=true&w=majority"
-const userSchema=require("./userSchema")
+const User=require("./userSchema")
 // https://mess-management038.herokuapp.com/
 mongoose.connect(mongodb,()=>{
     console.log("database connected")
 })
 
 app.get('/', (req, res) => {
+  console.log('ohh bhaaii')
   res.json({
     msg:"Hello Bhai2"
   })
 })
 
 app.get("/user",async(req,res)=>{
-    const user=await userSchema.find({})
+    const user=await User.find({})
     res.json(user)
+})
+app.post("/adduser",async(req,res)=>{
+  const newUser=new User(req.body)
+  console.log(req.body)
+  await newUser.save()
+  res.json({"msg":"ok"})
 })
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//git init 
+// git add .
+// git commit -m "msg"
+// git push heroku master
--- userSchema.js
+++ userSchema.js
@@ -1,13 +1,20 @@
 const mongoose=require("mongoose");
 const studentSchema=new mongoose.Schema({
-    task:{
+    name:{
         type:String,
         required:true
     },
     email:{
         type:String,
         required:true,
+        unique:true
     },
+    phone:{
+        type:String,
+    },
+    role:{
+        type:String
+    }
 },{timestamps:true})
 
 const Student=new mongoose.model("user",studentSchema);
